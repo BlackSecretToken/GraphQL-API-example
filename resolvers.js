ca990d12d799283869d8b75ef7b3c466ace8e0e3
BlackSecretToken <BenjaminJo7204@gmail.com>
2022-09-05 12:08:02 +0300

    Create Mutation

--- resolvers.js
+++ resolvers.js
@@ -1,4 +1,13 @@
+const db = require('./db')
+const Mutation = {
+   createStudent:(root,args,context,info) => {
+      return db.students.create({collegeId:args.collegeId,
+      firstName:args.firstName,
+      lastName:args.lastName})
+   }
+}
 const Query = {
-    test: () => 'Test Success, GraphQL server is up & running !!'
- }
- module.exports = {Query}
+   greeting:() => "hello"
+}
+
+module.exports = {Query,Mutation}
